2aca170a0313cce2575e53bde6c733d503d3e9cf
zijies1 <546641464@qq.com>
2019-08-10 12:36:51 +1000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FET-Web-Backend
+
+cd src
+virtualenv venv
+pip3 install -r requirements.txt
+export FlASK_APP=backend.py
+run flask
---
+++ src/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request, Response
+from flask_cors import CORS
+import json
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/test/hello', methods=['GET'])
+def hellof():
+	return "hello"
+
+if __name__ == '__main__':
+	# app.run(host='0.0.0.0',port=8081)
+	app.run()
